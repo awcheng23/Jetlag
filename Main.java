a01f9c97138dfdcc6f7bd26120acd841cc91b627
awcheng23 <awcheng23@gmail.com>
2023-11-16 08:59:23 -0500

    Add JavaFX stage framework

--- Main.java
+++ Main.java
@@ -1,1 +1,31 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
 
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+
+    Button btn = new Button();
+    btn.setText("Say 'Hello World'");
+    btn.setOnAction(e -> System.out.println("Hello World!"));
+
+    VBox layout = new VBox(20);
+    layout.setAlignment(Pos.CENTER);
+    layout.getChildren().addAll(btn);
+    Scene scene = new Scene(layout, 800, 600);
+
+    primaryStage.setTitle("Jetlag");
+    primaryStage.setMaximized(true);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
