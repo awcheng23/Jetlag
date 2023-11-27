e0ecf0035910e95a18b92f0555e281fe564593be
awcheng23 <awcheng23@gmail.com>
2023-11-27 10:49:59 -0500

    Original

--- plane_data.py
+++ plane_data.py
@@ -1,33 +1,19 @@
 import requests
-import json
 
 params = {
-  'access_key': 'd5890c79bb6123fa6f309e7c6dd5bae0'
+  'access_key': 'eee653bfbcd13d8d839a5d54c0fdd9f5'
 }
 
 api_result = requests.get('https://api.aviationstack.com/v1/flights', params)
 
 api_response = api_result.json()
 
-# for flight in api_response['results']:
-#     if (flight['live']['is_ground'] is False):
-#         print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
-#             flight['airline']['name'],
-#             flight['flight']['iata'],
-#             flight['departure']['airport'],
-#             flight['departure']['iata'],
-#             flight['arrival']['airport'],
-#             flight['arrival']['iata']))
-
-
-# Example JSON file: data.json
-# {"name": "John", "age": 30, "city": "New York"}
-
-# Load JSON data from a file
-#data = json.load(api_response)
-
-# Display the parsed data
-print(api_response)
-
-
-#print(api_response)
+for flight in api_response['results']:
+    if (flight['live']['is_ground'] is False):
+        print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
+            flight['airline']['name'],
+            flight['flight']['iata'],
+            flight['departure']['airport'],
+            flight['departure']['iata'],
+            flight['arrival']['airport'],
+            flight['arrival']['iata']))
