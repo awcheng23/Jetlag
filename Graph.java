a901d37a48646f9edbe6197148818cd0791a2e31
awcheng23 <awcheng23@gmail.com>
2023-11-28 00:59:22 -0500

    Added graph code

--- Graph.java
+++ Graph.java
@@ -31,26 +31,44 @@ public class Graph {
 
     public void findShortestPath(String source, String destination) {
         int i = 0;
-        HashMap<String, Integer> ID = new HashMap<>();
-        for(String key : adjacencyList.keySet()){
-            ID.put(key, i);
-            i++;
-        }
-
-        Node[] paths = new Node[ID.size()];
-        Arrays.fill(paths, new Node("", Integer.MAX_VALUE));
-        paths[ID.get(source)] = new Node("", 0);
+        HashMap<String, Node> paths = new HashMap<>();
+        for(String key : adjacencyList.keySet())
+            paths.put(key, new Node("", Integer.MAX_VALUE));
+        
+        paths.put(source, new Node("", 0));
         Set<String> visited = new HashSet<>();
         String curr = source;
         int length = 0;
         
     }
 
-    private void pathHelper(String curr, int length, Set visited, Node[] paths, String destination) {
+    private void pathHelper(String curr, int length, Set<String> visited, HashMap<String, Node> paths, String destination) {
         if(curr.equals(destination))
             return;
         
         visited.add(curr);
+        for(Node n : adjacencyList.get(curr)) {
+            int newLength = length + n.cost;
+            if(newLength < paths.get(n.data).cost)
+                paths.put(n.data, new Node(curr, newLength));
+        }
+
+        String next = findMinValueKey(paths);
+
+        pathHelper(next, paths.get(next).cost, visited, paths, destination);
+    }
+
+    private static String findMinValueKey(HashMap<String, Node> map) {
+        String minKey = "";
+        int minValue = Integer.MAX_VALUE;
+
+        for(String key : map.keySet()) {
+            if(map.get(key).cost < minValue) {
+                minValue = map.get(key).cost;
+                minKey = key;
+            }
+        }
+        return minKey;
     }
 }
 
