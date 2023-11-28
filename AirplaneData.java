0ff70f9d6552dd11066f2906a494ab9e980c7943
awcheng23 <awcheng23@gmail.com>
2023-11-27 20:41:45 -0500

    Read flight.csv

--- AirplaneData.java
+++ AirplaneData.java
@@ -1,34 +1,33 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class AirplaneData {
 
-    public static void main(String[] args) {
-        // Specify the path to your CSV file
-        String csvFile = "path/to/your/file.csv";
+    public static void main(String[] args) { // need to make not main
+        String csvFile = "flight.csv";
+
+        ArrayList<String[]> flights = new ArrayList<>(); // each flight
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            // Read the header line if it contains column names
-            String headerLine = br.readLine();
-            // You can use the headerLine to identify columns or skip it if not needed
+            // Read column names and store indices into HashMap
+            String[] headers = br.readLine().split(",");
+            HashMap<String, Integer> col = new HashMap<>();
+            for(int i=0; i<headers.length; i++) {
+                col.put(headers[i], i);
+            }
 
             // Read the rest of the lines
             String line;
             while ((line = br.readLine()) != null) {
-                // Split the line into an array of values using the comma as a delimiter
                 String[] values = line.split(",");
-
-                // Now you can access each value using the array index
-                String airlineName = values[0];
-                String travelTime = values[1];
-                // ... continue for other columns
-
-                // Print or process the values as needed
-                System.out.println("Airline: " + airlineName + ", Travel Time: " + travelTime);
+                flights.add(values);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.println(Arrays.toString(flights.get(0)));
     }
 }
