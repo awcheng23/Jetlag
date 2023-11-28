de21a20cb1f3ab333a961cff0327e566b2232722
awcheng23 <awcheng23@gmail.com>
2023-11-28 01:57:02 -0500

    Completed Dijkstra's Algorithm

--- Graph.java
+++ Graph.java
@@ -16,6 +16,11 @@ public class Graph {
             this.data = data;
             this.cost = cost;
         }
+
+        @Override
+        public String toString() {
+            return data+":"+cost;
+        }
     }
 
     // Add an aiport
@@ -26,6 +31,8 @@ public class Graph {
 
     // Add a path
     public void addEdge(String source, String destination, int cost) {
+        if(!adjacencyList.containsKey(source))
+            throw new NoSuchElementException();
         adjacencyList.get(source).add(new Node(destination, cost));
     }
 
@@ -46,13 +53,15 @@ public class Graph {
         Node end = paths.get(destination);
         result[0] = Integer.toString(end.cost);
 
-        String path = end.data;
+        String path = destination;
         while(!"".equals(end.data)){
-            end = paths.get(end.data);
             path = end.data + "," + path;
+            end = paths.get(end.data);
         }
 
         result[1] = path;
+
+        System.out.println(paths);
         
         return result;
     }
@@ -84,6 +93,24 @@ public class Graph {
             }
         }
         return minKey;
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        g.addVertex("A"); g.addVertex("B"); g.addVertex("C"); g.addVertex("D"); 
+        g.addVertex("E"); g.addVertex("F"); g.addVertex("G"); g.addVertex("H");
+        g.addEdge("A", "B", 6); g.addEdge("A", "C", 2); g.addEdge("A", "F", 4); g.addEdge("A", "G", 5); g.addEdge("A", "H", 6);
+        g.addEdge("B", "A", 6); g.addEdge("B", "D", 5); g.addEdge("B", "E", 2); g.addEdge("B", "F", 8);
+        g.addEdge("C", "A", 2); g.addEdge("C", "G", 3); g.addEdge("C", "H", 8);
+        g.addEdge("D", "B", 5); g.addEdge("D", "F", 7); 
+        g.addEdge("E", "F", 6); g.addEdge("E", "H", 11); g.addEdge("E", "B", 2);
+        g.addEdge("F", "A", 4); g.addEdge("F", "B", 8); g.addEdge("F", "D", 7); g.addEdge("F", "E", 6); g.addEdge("F", "G", 3);
+        g.addEdge("G", "A", 5); g.addEdge("G", "C", 3); g.addEdge("G", "H", 2); g.addEdge("G", "F", 3);
+        g.addEdge("H", "A", 6); g.addEdge("H", "C", 8); g.addEdge("H", "G", 2); g.addEdge("H", "E", 11);
+
+        String[] ans = g.findShortestPath("C", "D");
+
+        System.out.println(ans[1]);
     }
 }
 
