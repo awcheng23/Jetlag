705539ac45e20dc22581caf6f3f9f7560e6036e4
awcheng23 <awcheng23@gmail.com>
2023-11-27 21:18:04 -0500

    Basic framework

--- Graph.java
+++ Graph.java
@@ -1,28 +1,32 @@
 import java.util.*;
 
 public class Graph {
-    private Map<String, List<Node<String>>> adjacencyList;
+    private Map<String, List<Node>> adjacencyList;
 
     // Constructor
     public Graph() {
         this.adjacencyList = new HashMap<>();
     }
 
-    private class Node<String> {
-        
+    private class Node {
+        public String data;
+        public int cost;
+
+        public Node(String data, int cost){
+            this.data = data;
+            this.cost = cost;
+        }
     }
 
-    // Add a vertex to the graph
-    public void addVertex(int vertex) {
-        adjacencyList.put(vertex, new LinkedList<>());
+    // Add an aiport
+    public void addVertex(String source) {
+        if(!adjacencyList.containsKey(source))
+            adjacencyList.put(source, new LinkedList<>());
     }
 
-    // Add an edge to the graph
-    public void addEdge(int source, int destination) {
-        // Add the edge from source to destination
-        adjacencyList.get(source).add(destination);
-        // For an undirected graph, you might want to add the reverse edge as well
-        // adjacencyList.get(destination).add(source);
+    // Add a path
+    public void addEdge(String source, String destination, int cost) {
+        adjacencyList.get(source).add(new Node(destination, cost));
     }
 
 }
