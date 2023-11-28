7b063ca059fa145fc7e3a264270d14dc891a49f5
awcheng23 <awcheng23@gmail.com>
2023-11-27 21:49:48 -0500

    Initiated framework for Dijkstra's

--- Graph.java
+++ Graph.java
@@ -29,5 +29,28 @@ public class Graph {
         adjacencyList.get(source).add(new Node(destination, cost));
     }
 
+    public void findShortestPath(String source, String destination) {
+        int i = 0;
+        HashMap<String, Integer> ID = new HashMap<>();
+        for(String key : adjacencyList.keySet()){
+            ID.put(key, i);
+            i++;
+        }
+
+        Node[] paths = new Node[ID.size()];
+        Arrays.fill(paths, new Node("", Integer.MAX_VALUE));
+        paths[ID.get(source)] = new Node("", 0);
+        Set<String> visited = new HashSet<>();
+        String curr = source;
+        int length = 0;
+        
+    }
+
+    private void pathHelper(String curr, int length, Set visited, Node[] paths, String destination) {
+        if(curr.equals(destination))
+            return;
+        
+        visited.add(curr);
+    }
 }
 
