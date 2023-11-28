428e45ff17d989bca48a0f477bf9d520d885128e
awcheng23 <awcheng23@gmail.com>
2023-11-27 19:44:33 -0500

    Cleaned up csv

--- AirplaneData.java
+++ AirplaneData.java
@@ -1,36 +1,34 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class AirplaneData {
-    public static void main(String[] args) throws Exception {
 
-        String urlString = "https://api.aviationstack.com/v1/flights";
+    public static void main(String[] args) {
+        // Specify the path to your CSV file
+        String csvFile = "path/to/your/file.csv";
 
-        URL url = new URL(urlString);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            // Read the header line if it contains column names
+            String headerLine = br.readLine();
+            // You can use the headerLine to identify columns or skip it if not needed
 
-        String apiKey = "access_key:d5890c79bb6123fa6f309e7c6dd5bae0";
-        conn.setRequestProperty("Authorization", apiKey);
-        conn.setRequestMethod("GET");
+            // Read the rest of the lines
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Split the line into an array of values using the comma as a delimiter
+                String[] values = line.split(",");
 
-        System.out.println(conn.getResponseCode());
+                // Now you can access each value using the array index
+                String airlineName = values[0];
+                String travelTime = values[1];
+                // ... continue for other columns
 
-
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line;
-        StringBuilder result = new StringBuilder();
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
+                // Print or process the values as needed
+                System.out.println("Airline: " + airlineName + ", Travel Time: " + travelTime);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        rd.close();
-
-        // The result variable now contains the raw JSON response.
-        // You'll need to parse this JSON string manually as Java's standard library doesn't have a built-in JSON parser.
-        // Consider using a library like Gson or Jackson if you need to parse JSON frequently.
-        //System.out.println(result.toString());
     }
 }
