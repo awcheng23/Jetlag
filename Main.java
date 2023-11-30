9c170f4c2f00c9dc0f9efd0b7521db4c3180510c
awcheng23 <awcheng23@gmail.com>
2023-11-30 09:13:18 -0500

    Add style

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -25,11 +29,21 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) {
 
-    Font.getFamilies(); 
+    
 
     Label name = new Label();
     name.setText("Jetlag");
     name.setFont(Font.font("Century", 45));
+
+    Image gif = new Image("graphics/plane.gif");
+    ImageView gv = new ImageView(gif);
+    gv.setPreserveRatio(true);
+    gv.setFitWidth(100);
+
+    HBox titleBox = new HBox();
+    titleBox.getChildren().addAll(name, gv);
+    titleBox.setSpacing(20);
+    titleBox.setAlignment(Pos.CENTER);
 
     Image backgroundImage = new Image("graphics/cloud-bg.jpg");
     BackgroundImage background = new BackgroundImage(backgroundImage, null, null, null, 
@@ -39,10 +53,11 @@ public class Main extends Application {
 
     VBox vBox = new VBox(20);
     vBox.setBackground(new Background(background));
-    vBox.getChildren().addAll(name);
+    vBox.getChildren().addAll(titleBox);
     vBox.setAlignment(Pos.CENTER);
     
     Scene scene = new Scene(vBox, 800, 600);
+    // scene.getStylesheets().add("graphics/style.css");
 
     primaryStage.setTitle("Jetlag");
     primaryStage.setMaximized(true);
