755a1fdf76224b7db88e38e5f66788143490e07f
awcheng23 <awcheng23@gmail.com>
2023-11-30 08:42:25 -0500

    add background

--- Main.java
+++ Main.java
@@ -2,22 +2,44 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.geometry.Pos;
 
+/**
+ * Jetlag Application
+ *
+ * @author May Ming, Alice Cheng
+ * @version 0.0.1
+ */
 public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) {
 
-    Button btn = new Button();
-    btn.setText("Say 'Hello World'");
-    btn.setOnAction(e -> System.out.println("Hello World!"));
+    Label name = new Label();
+    name.setText("Jetlag");
+    name.setFont(Font.font("Lucida Bright", 45));
 
-    VBox layout = new VBox(20);
-    layout.setAlignment(Pos.CENTER);
-    layout.getChildren().addAll(btn);
-    Scene scene = new Scene(layout, 800, 600);
+    Image backgroundImage = new Image("graphics/cloud-bg.jpg");
+    BackgroundImage background = new BackgroundImage(backgroundImage, null, null, null, 
+                                                     new BackgroundSize(800, 600, true, true, true, true));
+   
+    
+
+    VBox vBox = new VBox(20);
+    vBox.setBackground(new Background(background));
+    vBox.getChildren().addAll(name);
+    vBox.setAlignment(Pos.CENTER);
+    
+    Scene scene = new Scene(vBox, 800, 600);
 
     primaryStage.setTitle("Jetlag");
     primaryStage.setMaximized(true);
