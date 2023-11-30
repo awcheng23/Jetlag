1ef43c03384061b955d1e8b3b6d7e8aff00574db
May Ming <ming24m@ncssm.edu>
2023-11-30 09:19:20 -0500

    Gridpane converted to Hboxs

--- test.java
+++ test.java
@@ -18,10 +18,10 @@ import javafx.scene.control.Label;
 public class test extends Application {
   @Override
   public void start(Stage primaryStage) {
-    GridPane input = new GridPane();
-    input.setPadding(new Insets(20));
-    input.setHgap(50);
-    input.setVgap(10);
+    HBox row1 = new HBox(20);
+    HBox row2 = new HBox(20);
+    row1.setPadding(new Insets(20));
+    row2.setPadding(new Insets(20));
 
     //labels
     Label label1 = new Label("Departure Airport");
@@ -35,20 +35,22 @@ public class test extends Application {
 
     Button submitButton = new Button("Find");
     
-    input.setAlignment(Pos.TOP_LEFT);
-    input.add(label1,0,0);
-    input.add(label2,1,0);
-    input.add(label3,2,0);
-    input.add(departureAirport,0,1);
-    input.add(departureTime,1,1);
-    input.add(arrivalAirport,2,1);
-    input.add(submitButton,3,1);
+    row1.setAlignment(Pos.TOP_LEFT);
+    row1.getChildren().addAll(label1,label2,label3);
+    row2.getChildren().addAll(departureAirport, departureTime, arrivalAirport,submitButton);
 
     //labels
-    GridPane output = new GridPane();
-    output.setPadding(new Insets(20));
-    output.setHgap(50);
-    output.setVgap(10);
+    HBox row3 = new HBox(10);
+    row3.setPadding(new Insets(10));
+
+    HBox row4 = new HBox(20);
+    row4.setPadding(new Insets(20));
+
+    HBox row5 = new HBox(10);
+    row5.setPadding(new Insets(10));
+
+    HBox row6 = new HBox(20);
+    row6.setPadding(new Insets(10));
 
     Label label4 = new Label("Departure Airport");
     Label label5 = new Label("Layover Airport");
@@ -61,16 +63,14 @@ public class test extends Application {
     Label label7 = new Label("Departure Time");
     Label label8 = new Label("Layover Time");
     Label label9 = new Label("Final Destination Arrival Time"); 
-
+    Label label10 = new Label("Price");
+    
     Label departureTimeOutput = new Label();
     Label layoverTimeOutput = new Label();
     Label arrivalTimeOutput = new Label();
-
-    Label label10 = new Label("Price");
-    Label label11 = new Label("Class");
-
     Label priceOutput = new Label();
-    Label classOutput = new Label();
+
+    Button newFlight = new Button("Find New Flight");
 
     submitButton.setOnAction(e -> {
       departureAirportOutput.setText(departureAirport.getText());
@@ -80,32 +80,35 @@ public class test extends Application {
       layoverTimeOutput.setText("9:00 am");
       arrivalTimeOutput.setText("10:00 pm");
       priceOutput.setText("$4040");
-      classOutput.setText("Economy");
     });
 
-    output.setAlignment(Pos.TOP_LEFT);
-    output.add(label4,0,0);
-    output.add(label5,1,0);
-    output.add(label6,2,0);
-    output.add(departureAirportOutput,0,1);
-    output.add(layoverAirportOutput,1,1);
-    output.add(arrivalAirportOutput,2,1);
-    output.add(label7,0,2);
-    output.add(label8,1,2);
-    output.add(label9,2,2);
-    output.add(departureTimeOutput,0,3);
-    output.add(layoverTimeOutput,1,3);
-    output.add(arrivalTimeOutput,2,3);
-    output.add(label10,0,4);
-    output.add(label11,1,4);
-    output.add(priceOutput,0,5);
-    output.add(classOutput,1,5);
+    newFlight.setOnAction(e -> {
+      departureAirportOutput.setText("");
+      layoverAirportOutput.setText("");
+      arrivalAirportOutput.setText("");
+      departureTimeOutput.setText("");
+      layoverTimeOutput.setText("");
+      arrivalTimeOutput.setText("");
+      priceOutput.setText("");
+    });
+
+    row3.setAlignment(Pos.TOP_LEFT);
+    row3.getChildren().addAll(label4,label5,label6);
+
+    row4.setAlignment(Pos.TOP_LEFT);
+    row4.getChildren().addAll(departureAirportOutput,layoverAirportOutput,arrivalAirportOutput);
+
+    row5.setAlignment(Pos.TOP_LEFT);
+    row5.getChildren().addAll(label7,label8,label9,label10,newFlight);
+
+    row6.setAlignment(Pos.TOP_LEFT);
+    row6.getChildren().addAll(departureTimeOutput,layoverTimeOutput,arrivalTimeOutput,priceOutput);
+
 
      // define scenes and containers
-    VBox layout = new VBox(20);
-    layout = new VBox(20);
+    VBox layout = new VBox(5);
     layout.setAlignment(Pos.TOP_CENTER);
-    layout.getChildren().addAll(input,output);
+    layout.getChildren().addAll(row1,row2,row3,row4,row5,row6);
 
     Scene scene = new Scene(layout, 800, 600);
     // show first scene
