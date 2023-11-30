d03960443b842190fcc7069c8e7856bbd6da5722
awcheng23 <awcheng23@gmail.com>
2023-11-30 12:10:32 -0500

    Add find and filter box

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 import java.io.FileInputStream;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
@@ -15,6 +19,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import java.util.*;
 
@@ -46,13 +51,135 @@ public class Main extends Application {
     titleBox.getChildren().addAll(name, gv);
     titleBox.setAlignment(Pos.CENTER);
 
+    HBox row1 = new HBox(20);
+    HBox row2 = new HBox(20);
+    //row1.setPadding(new Insets(20));
+    //row2.setPadding(new Insets(20));
+
+     //labels
+    Label label1 = new Label("Departure Airport");
+    Label label2 = new Label("Departure Time");
+    Label label3 = new Label("Arrival Airport");   
+
+    // Search boxes
+    ObservableList<String> departuresList = FXCollections.observableArrayList(departures);
+    ComboBox<String> departCBox = new ComboBox<>(departuresList);
+    departCBox.setEditable(true);
+
+    TextField departEditor = departCBox.getEditor();
+    departEditor.textProperty().addListener((observable, oldValue, newValue) -> {
+        // Filter the choices based on the entered text
+        ObservableList<String> filteredList = FXCollections.observableArrayList();
+        departuresList.stream()
+                .filter(choice -> choice.toLowerCase().contains(newValue.toLowerCase()))
+                .forEach(filteredList::add);
+
+        // Update the items in the ComboBox
+        departCBox.setItems(filteredList);
+        departCBox.show();
+    });
+
+    ObservableList<String> arrivalsList = FXCollections.observableArrayList(arrivals);
+    ComboBox<String> arriveCBox = new ComboBox<>(arrivalsList);
+    arriveCBox.setEditable(true);
+
+    TextField arriveEditor = arriveCBox.getEditor();
+    arriveEditor.textProperty().addListener((observable, oldValue, newValue) -> {
+        // Filter the choices based on the entered text
+        ObservableList<String> filteredList = FXCollections.observableArrayList();
+        arrivalsList.stream()
+                .filter(choice -> choice.toLowerCase().contains(newValue.toLowerCase()))
+                .forEach(filteredList::add);
+
+        // Update the items in the ComboBox
+        arriveCBox.setItems(filteredList);
+        arriveCBox.show();
+    });
+
+    // TextField departureAirport = new TextField("Departure Airport");
+    // TextField departureTime = new TextField("Departure Time");
+    // TextField arrivalAirport = new TextField("Arrival Airport");
+
+    Button submitButton = new Button("Find");
+    
+    
+    //row1.getChildren().addAll(label1,label2,label3);
+    row2.getChildren().addAll(departCBox, arriveCBox ,submitButton);
+   // row1.setAlignment(Pos.CENTER);
+    row2.setAlignment(Pos.CENTER);
+
+    //labels
+    HBox row3 = new HBox(10);
+    //row3.setPadding(new Insets(10));
+
+    HBox row4 = new HBox(20);
+    //row4.setPadding(new Insets(20));
+
+    HBox row5 = new HBox(10);
+    //row5.setPadding(new Insets(10));
+
+    HBox row6 = new HBox(20);
+    //row6.setPadding(new Insets(10));
+
+    Label label4 = new Label("Departure Airport");
+    Label label5 = new Label("Layover Airport");
+    Label label6 = new Label("Final Destination Airport");
+
+    Label departureAirportOutput = new Label();
+    Label layoverAirportOutput = new Label();
+    Label arrivalAirportOutput = new Label();
+
+    Label label7 = new Label("Departure Time");
+    Label label8 = new Label("Layover Time");
+    Label label9 = new Label("Final Destination Arrival Time"); 
+    Label label10 = new Label("Price");
+    
+    Label departureTimeOutput = new Label();
+    Label layoverTimeOutput = new Label();
+    Label arrivalTimeOutput = new Label();
+    Label priceOutput = new Label();
+
+    Button newFlight = new Button("Find New Flight");
+
+    submitButton.setOnAction(e -> {
+      departureAirportOutput.setText(departCBox.getValue());
+      layoverAirportOutput.setText(departCBox.getValue());
+      arrivalAirportOutput.setText(arriveCBox.getValue());
+      departureTimeOutput.setText("8:00 pm");
+      layoverTimeOutput.setText("9:00 am");
+      arrivalTimeOutput.setText("10:00 pm");
+      priceOutput.setText("$4040");
+    });
+
+    newFlight.setOnAction(e -> {
+      departureAirportOutput.setText("");
+      layoverAirportOutput.setText("");
+      arrivalAirportOutput.setText("");
+      departureTimeOutput.setText("");
+      layoverTimeOutput.setText("");
+      arrivalTimeOutput.setText("");
+      priceOutput.setText("");
+    });
+
+    
+    row3.getChildren().addAll(label4,label5,label6);
+    row3.setAlignment(Pos.CENTER);
+    
+    row4.getChildren().addAll(departureAirportOutput,layoverAirportOutput,arrivalAirportOutput);
+    row4.setAlignment(Pos.CENTER);
+    
+    row5.getChildren().addAll(label7,label8,label9,label10,newFlight);
+    row5.setAlignment(Pos.CENTER);
+    
+    row6.getChildren().addAll(departureTimeOutput,layoverTimeOutput,arrivalTimeOutput,priceOutput);
+    row6.setAlignment(Pos.CENTER);
+
     Image backgroundImage = new Image("graphics/cloud-bg.jpg");
     BackgroundImage background = new BackgroundImage(backgroundImage, null, null, null, 
                                                      new BackgroundSize(800, 600, true, true, true, true));
-    
     VBox vBox = new VBox(20);
     vBox.setBackground(new Background(background));
-    vBox.getChildren().addAll(titleBox);
+    vBox.getChildren().addAll(titleBox, row2,row3,row4,row5,row6);
     vBox.setAlignment(Pos.CENTER);
     
     Scene scene = new Scene(vBox, 800, 600);
