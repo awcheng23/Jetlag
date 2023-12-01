40b8c3ffa06b33e65f810b0e0ad1865cfd86c066
awcheng23 <awcheng23@gmail.com>
2023-11-30 20:02:07 -0500

    Fix stops

--- FlightData.java
+++ FlightData.java
@@ -17,8 +17,8 @@ public class FlightData {
     private Set<String> arrivalAirports; // Set of arrival airports
     private Graph routes; // Graph of all connecting flights
 
-    private final String[] DEPARTURE_STOPS = {"Departure Airport", "1st Stop", "2nd Stop", "3rd Stop"};
-    private final String[] ARRIVAL_STOPS = {"Arrival Airport"};
+    private final String[] DEPARTURE_STOPS = {"Departure Airport"};
+    private final String[] ARRIVAL_STOPS = {"1st Stop", "2nd Stop", "3rd Stop", "Arrival Airport"};
 
     /**
      * Retrieves flight data from a CSV file
